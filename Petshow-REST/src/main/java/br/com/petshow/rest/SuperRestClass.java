44d7b43e261ac40ea2cfca90f3a48bcfb3069eec
antoniorafael <rafasystec@yahoo.com.br>
2017-02-07 12:36:31 -0300

    Para o desenpenho

--- Petshow-REST/src/main/java/br/com/petshow/rest/SuperRestClass.java
+++ Petshow-REST/src/main/java/br/com/petshow/rest/SuperRestClass.java
@@ -1,12 +1,13 @@
 package br.com.petshow.rest;
 
-import java.util.Date;
-
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+
+import br.com.petshow.util.WriteConsoleUtil;
 
 /**
  * Super Classe para reutilizar metodos nas outras classes REST
@@ -16,8 +17,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SuperRestClass {
 
 
-	private ApplicationContext context;//= new ClassPathXmlApplicationContext("spring-context.xml")
-
+	private static ApplicationContext context;//= new ClassPathXmlApplicationContext("spring-context.xml")
+//	private XmlWebApplicationContext context;
+	
 	public SuperRestClass() {
 		// TODO Auto-generated constructor stub
 		//context = new ClassPathXmlApplicationContext("spring-context.xml");
@@ -29,7 +31,7 @@ public class SuperRestClass {
 //	}
 	
 	static{
-		//context = new ClassPathXmlApplicationContext("spring-context.xml");
+		context = new ClassPathXmlApplicationContext("spring-context.xml");
 	}
 	
 	
@@ -53,16 +55,35 @@ public class SuperRestClass {
 	}
 
 
+	
 	public void setContext() {
-		System.out.println("trace descobrir lentidao:entrou super class1:"+new Date().getTime());
-		this.context =new ClassPathXmlApplicationContext("spring-context.xml");
-		System.out.println("trace descobrir lentidao:entrou super class2:"+new Date().getTime());
+		/**
+		 * In non-web applications we use ApplicationContext interface to provide beans configuration to our applications 
+		 * from xml files.For example:
+			ApplicationContext context = new FileSystemXmlApplicationContext("c:/foo.xml");
+			OR
+			ApplicationContext context = new ClassPathXmlApplicationContext("foo.xml");
+			------------------------------------------------------------------------------
+						FOR WEB APPLICATIONS
+			------------------------------------------------------------------------------
+			But in web applications we use WebApplicationContext interface instead of ApplicationContext interface to 
+			provide bean configuration to our web application and to represent the spring container.
+			WebApplicationContext extends ApplicationContext and has some extra features necessary for web applications.
+		 */
+		if(context == null){
+			WriteConsoleUtil.write("Contexto do Spring ainda NÃO iniciado - Comecando inicializacao");
+			context =new ClassPathXmlApplicationContext("spring-context.xml");
+//			this.context =new XmlWebApplicationContext();
+//			this.context.refresh();
+			WriteConsoleUtil.write("******* OK - spring-context Iniciado ********** ");
+		}
 	}
 	
-	public void setContext(ApplicationContext context) {
-		this.context = context;
-	
-	}
+//	public void setContext(ApplicationContext context) {
+//	public void setContext(XmlWebApplicationContext context) {
+//		context = context;
+//	
+//	}
 	public void inicializar(){
 		setContext();
 	}
