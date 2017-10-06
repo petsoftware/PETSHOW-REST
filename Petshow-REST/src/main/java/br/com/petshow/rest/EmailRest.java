ef428521517de7c74d87030bc9de08a68d863e0b
rafasystec <rafasystec@yahoo.com.br>
2017-10-06 10:56:00 -0300

    Ajustes para o site e as tarefas

--- Petshow-REST/src/main/java/br/com/petshow/rest/EmailRest.java
+++ Petshow-REST/src/main/java/br/com/petshow/rest/EmailRest.java
@@ -1,33 +1,35 @@
 package br.com.petshow.rest;
 
-import java.util.HashMap;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Component;
 
+import br.com.petshow.objects.EnviarMensagem;
+import br.com.petshow.role.RoleSendEmail;
 import br.com.petshow.util.RestUtil;
 @Component
 @Path("/email")
 public class EmailRest extends SuperRestClass{
 	
-	
+	private RoleSendEmail roleSendEmail;
 	
 	@POST
 	@Path("enviar")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response enviarEmail(HashMap<String,String> parametros){
-
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response enviarEmail(EnviarMensagem enviarMensagem){
+		roleSendEmail = getContext().getBean(RoleSendEmail.class);
 		try{
-		
+			roleSendEmail.enviar(enviarMensagem);
 		} catch (Exception e) {
 			return RestUtil.getResponseErroInesperado(e);
 		}
-		return Response.ok().build();
+		return Response.ok().entity(enviarMensagem).build();
 		
 	}
 	
--- Petshow-REST/src/main/java/br/com/tarefa/rest/TestCargaRest.java
+++ Petshow-REST/src/main/java/br/com/tarefa/rest/TestCargaRest.java
@@ -1,7 +1,7 @@
 package br.com.tarefa.rest;
 
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -17,10 +17,11 @@ import org.springframework.stereotype.Component;
 
 import br.com.petshow.enums.EnumAchadoPerdido;
 import br.com.petshow.enums.EnumSexo;
-import br.com.petshow.enums.EnumTipoAnimal;
 import br.com.petshow.exceptions.ExceptionValidation;
+import br.com.petshow.model.Animal;
 import br.com.petshow.model.Perdido;
 import br.com.petshow.rest.SuperRestClass;
+import br.com.petshow.role.AnimalRole;
 import br.com.petshow.role.PerdidoRole;
 import br.com.petshow.util.RestUtil;
 import br.com.tarefa.model.Tarefa;
@@ -29,8 +30,8 @@ import br.com.tarefa.role.TarefaRole;
 
 
 @Component
-@Path("/tarefa")
-public class TarefaRest  extends SuperRestClass {
+@Path("/teste")
+public class TestCargaRest  extends SuperRestClass {
 
 	TarefaRole tarefaRole;
 	PerdidoRole perdidoRole;
@@ -38,8 +39,11 @@ public class TarefaRest  extends SuperRestClass {
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response consultaTarefa(@PathParam("id") long id){
+
 		Tarefa entidade=null;
 		try {
+		
+
 			tarefaRole = getContext().getBean(TarefaRole.class);
 			entidade= tarefaRole.find(id);
 
@@ -48,44 +52,43 @@ public class TarefaRest  extends SuperRestClass {
 		} catch (Exception e) {
 			return RestUtil.getResponseErroInesperado(e);
 		}
+
+
 		return Response.ok().entity(entidade).build();
 	}
 
 
 	@POST
-	@Path("salvar")
+	@Path("carga/perdido")
 	@Produces("application/json")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response salvarTarefa(Tarefa tarefa){
-		tarefaRole = getContext().getBean(TarefaRole.class);
-		if(tarefa.getId()>0){
-			try {
-				tarefaRole.update(tarefa);
 
-			} catch (ExceptionValidation e) {
+	
 
-				return RestUtil.getResponseValidationErro(e);
-
-			}catch (Exception e) {
-
-				return RestUtil.getResponseErroInesperado(e);
-
+		perdidoRole = getContext().getBean(PerdidoRole.class);
+		try {
+			Perdido perdido = perdidoRole.find(1L);
+			if(perdido != null){
+				for (int i = 0; i < 4; i++) {
+					Perdido perdido2 = new Perdido();
+					perdido2.setDataCadastro(new Date());
+					perdido2.setDddCelular(0);
+					perdido2.setDddResidencial(0);
+					perdido2.setDescAcontecimento("Perdido teste de carga " + i);
+					perdido2.setFlAcontecimento(EnumAchadoPerdido.PERDIDO);
+					perdido2.setFlAtivo(true);
+					perdido2.setFlSexo(EnumSexo.MACHO);
+					perdido2.setFotos(perdido.getFotos());
+					perdido2.setNome("Id na posição " + i);
+					perdidoRole.insert(perdido2);
+				}
 			}
-		}else{
-			try {
-				tarefaRole.insert(tarefa);
-
-			} catch (ExceptionValidation e) {
-
-				return RestUtil.getResponseValidationErro(e);
-
-			}catch (Exception e) {
-
-				return RestUtil.getResponseErroInesperado(e);
-
-			}
-
+		} catch (ExceptionValidation e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
 		return Response.ok().entity(tarefa).build();
 
 
@@ -96,7 +99,11 @@ public class TarefaRest  extends SuperRestClass {
 	@Produces("application/json")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response alterarStatus(Tarefa tarefa){
+
+		
+
 		tarefaRole = getContext().getBean(TarefaRole.class);
+
 		try {
 			tarefaRole.alterarStatus(tarefa);
 
@@ -110,6 +117,8 @@ public class TarefaRest  extends SuperRestClass {
 
 		}
 		return Response.ok().entity(tarefa).build();
+
+
 	}
 
 	@GET
@@ -137,6 +146,8 @@ public class TarefaRest  extends SuperRestClass {
 	@Path("todas")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response tarefas(){
+
+		
 		List<Tarefa> tarefas =null;
 		try {
 			tarefaRole = getContext().getBean(TarefaRole.class);
@@ -147,53 +158,7 @@ public class TarefaRest  extends SuperRestClass {
 			return RestUtil.getResponseErroInesperado(e);
 		}
 		return Response.ok(tarefas).build();
-	}
-	
-	public void testeDecargaPerdido() {
-		perdidoRole = getContext().getBean(PerdidoRole.class);
-		try {
-			Perdido perdido = perdidoRole.find(120002L);
-			if(perdido != null){
-				int total = 0;
-				for (int i = 0; i < 10000; i++) {
-					Perdido perdido2 = new Perdido();
-					perdido2.setDataCadastro(new Date());
-					perdido2.setDddCelular(0);
-					perdido2.setDddResidencial(0);
-					perdido2.setDescAcontecimento(perdido.getNome() + "-" + i);
-					perdido2.setFlAcontecimento(EnumAchadoPerdido.PERDIDO);
-					perdido2.setFlAtivo(true);
-					perdido2.setFlSexo(EnumSexo.MACHO);
-					
-					List<String> fotos = new ArrayList<>();
-					for (String foto : perdido.getFotos()) {
-						fotos.add(foto);
-					}
-					perdido2.setFotos(fotos);
-					perdido2.setNome("Id na posição " + i);
-					perdido2.setDtPerdidoAchado(new Date());
-					perdido2.setTpAnimal(EnumTipoAnimal.CACHORRO);
-					perdido2.setTelefoneCelular(855995566L);
-					
-					perdidoRole.insert(perdido2);
-					total++;
 
-				}
-				System.out.println("registros inseridos " + total);
-			}
-		} catch (ExceptionValidation e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
-	
-	@GET
-	@Path("carga")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response cargaDeTabelas(){
-		testeDecargaPerdido();
-		return Response.ok().build();
-	}
-
 
 }
